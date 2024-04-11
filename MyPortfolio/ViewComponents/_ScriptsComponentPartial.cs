d3de86b1393c8a1ff098a0580dcc64e5b05ae8a4
Baran Gungor <barangungor33@gmail.com>
2024-04-11 12:07:40 +0300

    Tema Dosyaları Yüklendi ve View Component ile düzenlendi.

---
+++ MyPortfolio/ViewComponents/_ScriptsComponentPartial.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyPortfolio.ViewComponents
+{
+    public class _ScriptsComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke() { return View(); }
+    }
+}
